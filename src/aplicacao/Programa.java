2253c8ffed2acd69e6ded1a0b79e860deb03a035
Tiago Cordeiro <tiagoezc@hotmail.com>
2022-10-22 16:47:49 -0300

    solucao ruim

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -25,24 +25,36 @@ public class Programa {
 		System.out.print("Data de saida (DD/MM/YYYY): ");
 		Date saida = sdf.parse(sc.next());
 		
-		Reserva reserva = new Reserva(numQuarto, entrada, saida);
-		System.out.println(reserva);
+		if (!saida.after(entrada)) {
+			System.out.println("Erro! Em uma reserva, a data de saida deve ser depois da data de entrada!");
+		} else {
+			
+			Reserva reserva = new Reserva(numQuarto, entrada, saida);
+			System.out.println(reserva);
+			
+			System.out.println();
+			System.out.println("Entre com os dados para atualizar a reserva: ");
+			
+			System.out.print("Data de entrada (DD/MM/YYYY): ");
+			entrada = sdf.parse(sc.next());
+			
+			System.out.print("Data de saida (DD/MM/YYYY): ");
+			saida = sdf.parse(sc.next());
+			
+			Date agora = new Date();
+			
+			if (entrada.before(agora) || saida.before(agora)) {
+				System.out.println("Erro na reserva, as datas de reserva na atualizacao devem ser datas futuras.");
+			} else if (!saida.after(entrada)) {
+				System.out.println("Erro! Em uma reserva, a data de saida deve ser depois da data de entrada!");
+			} else {
+				reserva.atualizarData(entrada, saida);
+				
+				System.out.println(reserva);
+			}
 		
-		System.out.println();
-		System.out.println("Entre com os dados para atualizar a reserva: ");
-		
-		System.out.print("Data de entrada (DD/MM/YYYY): ");
-		entrada = sdf.parse(sc.next());
-		
-		System.out.print("Data de saida (DD/MM/YYYY): ");
-		saida = sdf.parse(sc.next());
-		
-		reserva.atualizarData(entrada, saida);
-		
-		System.out.println(reserva);
-		
+		}
 		sc.close();
-
 	}
 
 }
